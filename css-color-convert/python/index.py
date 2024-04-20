ee23be56646dbfd6c51c64cba3ccc3be4d80707b
Charul <baricharul9@hotmail.com>
2024-04-20 12:24:10 +0530

    Refactor python solution of css color convert

--- css-color-convert/python/index.py
+++ css-color-convert/python/index.py
@@ -1,34 +1,41 @@
 import sys
 import re
 
+hex_to_decimal_dict = dict(zip('0123456789abcdef', range(16)))
 
-def convertHexToRGB(hex_string):
-    hex_string = hex_string.removeprefix('#')
+
+def getNormalisedHexString(hex_string):
     if len(hex_string) == 3 or len(hex_string) == 4:
         curr_string = ''
         for i in range(len(hex_string)):
             curr_string += hex_string[i] + hex_string[i]
         hex_string = curr_string
-    rgba_string = ''
-    for i in range(0, len(hex_string), 2):
-        curr_val = int(hex_string[i] + hex_string[i + 1], 16)
-        if i == 6:
-            rgba_string += '/ ' + str(round(curr_val / 255, 5))
-        else:
-            space_str = '' if i == len(hex_string) - 2 else ' '
-            rgba_string += str(curr_val) + space_str
 
-    rgba_string = 'rgba(' + rgba_string + ')' if len(hex_string) == 8 else 'rgb(' + rgba_string + ')'
+    return hex_string
+
+
+def getHexToDecimal(hex_string):
+    return hex_to_decimal_dict[hex_string[0].lower()] << 4 | hex_to_decimal_dict[hex_string[1].lower()]
+
+
+def convertHexToRGB(match_obj):
+    hex_string = getNormalisedHexString(match_obj[0].removeprefix('#'))
+    rgba_string = ''
+
+    rgba_list = []
+    for i in range(0, len(hex_string), 2):
+        curr_val = getHexToDecimal(hex_string[i]+hex_string[i+1])
+        if i == 6:
+            rgba_list.append('/')
+            rgba_list.append(str(round(curr_val / 255, 5)))
+        else:
+            rgba_list.append(str(curr_val))
+
+    rgba_prefix_string = 'rgba' if len(hex_string) == 8 else 'rgb'
+    rgba_string = f'{rgba_prefix_string}({" ".join(rgba_list)})'
+
     return rgba_string
 
 
-fileContent = sys.stdin.read()
-splitFileContent = re.split(r': |;', fileContent)
-
-for x in range(len(splitFileContent)):
-    if splitFileContent[x][0] == '#':
-        rgba = convertHexToRGB(splitFileContent[x])
-        splitFileContent[x] = ': ' + rgba + ';'
-
-sys.stdout.write(''.join(splitFileContent))
+sys.stdout.write(re.sub(r'#[0-9aA-fF]+', convertHexToRGB, sys.stdin.read()))
 
