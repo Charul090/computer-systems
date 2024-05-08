c822049688d009abb8ef6034b3e733fc5b156314
Charul <baricharul9@hotmail.com>
2024-05-08 22:41:34 +0530

    Update python solution

--- utf-8-truncate/python/index.py
+++ utf-8-truncate/python/index.py
@@ -1,31 +1,20 @@
 import sys
 
 
-def getBytesForUtf(code_point):
-    byte_limits = [[0, 127], [194, 223], [224, 239], [240, 247]]
-    for i in range(len(byte_limits)):
-        if byte_limits[i][0] <= code_point <= byte_limits[i][1]:
-            return i+1
+def handleTruncate(byte, length):
+    while length > 0 and (byte[length] & 0b11000000 == 0b10000000):
+        length -= 1
 
-    return 4
+    return byte[:length]
 
 
-def handleTruncate(file_obj):
-    for current_line in file_obj:
-        length = current_line[0]
-        string = current_line[1:]
-        x = 0
-        while x < len(string):
-            curr_byte_len = getBytesForUtf(string[x])
-            if length - curr_byte_len >= 0:
-                length -= curr_byte_len
-                sys.stdout.buffer.write(string[x:x+curr_byte_len])
-                x += curr_byte_len
-            else:
-                sys.stdout.buffer.write(b'\n')
-                break
-    sys.stdout.buffer.flush()
-
-
+# open the file as file object containing data as byte object
 with open('../files/cases', 'rb') as file_object:
-    handleTruncate(file_object)
+    for current_line in file_object:
+        target_length = current_line[0]
+        string = current_line[1:]
+        if target_length > len(string):
+            sys.stdout.buffer.write(string)
+            continue
+        sys.stdout.buffer.write(handleTruncate(string, target_length) + b'\n')
+
