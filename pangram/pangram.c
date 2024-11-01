998426c28aae566de628e812f5042043e7a5ca15
Charul <baricharul9@hotmail.com>
2024-11-01 23:49:49 +0530

    refactor pangram solution

--- pangram/pangram.c
+++ pangram/pangram.c
@@ -2,21 +2,23 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+#define MASK 0x7fffffe
 
 bool ispangram(char *s) {
+  // since the difference between the lowercase and uppercase alphabet's ascii value is 32, char_bits will maintain 32 bits
+  // and characters with ascii value lying between 64 and 127 will be masked with 31 to gets position withing 32 bit and that bit will be set
+  // the string will be pangram if bits 1 to 25 are set to 1. 0 & 26-31 are not relevant bits for pangram
+  // (hence the char_bits will be masked with 0x7fffffe to check if 1 to 25th bits are set or not).
   int char_bits = 0;
-  // either between 65-90 or 97-122
-  while(*s) {
-    int lower_diff = *s - 65;
-    int higher_diff = *s - 97;
-    if (lower_diff >= 0 && lower_diff <= 25) {
-      char_bits |= 1 << lower_diff;
-    } else if (higher_diff >= 0 && higher_diff <= 25) {
-      char_bits |= 1 << higher_diff;
+
+  while(*s != '\0') {
+    if (*s >= '@' && *s <= 127) {
+      char_bits |= 1 << (*s & 0x1f);
     }
     s++;
   }
-  return char_bits == ((2 << 25) - 1);
+
+  return (char_bits & MASK) == MASK;
 }
 
 int main() {
