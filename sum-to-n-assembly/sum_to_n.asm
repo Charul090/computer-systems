feed7f082d46fec70f3ac6f9875f8e3984b62f86
Charul <baricharul9@hotmail.com>
2024-12-28 19:29:29 +0530

    add O(1) solution to sum_to_n in assembly

--- sum-to-n-assembly/sum_to_n.asm
+++ sum-to-n-assembly/sum_to_n.asm
@@ -2,14 +2,11 @@ section .text
 global sum_to_n
 
 sum_to_n:
-	xor rax,rax ;make rax 0
-	mov rbx,rdi ;copy first argument(rdi) to rbx
-	cmp rbx, 0 ;check if rbx is greater than 0
-	jg loop_to_n ; execute loop_to_n if it is
-	ret ;return if its not
-loop_to_n:
-	add rax,rbx
-	dec rbx
-	cmp rbx,0 ;check if rbx is greater than 0
-	jg loop_to_n ;if it is jump to loop_to_n to run it again
-	ret ;else return rax which is sum of n now
+	xor rax,rax
+	mov rax,rdi
+	mov rbp,rax
+	add rbp,1 
+	imul rax,rbp
+	shr rax, 1
+	ret
+
